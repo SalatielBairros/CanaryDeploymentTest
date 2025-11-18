d01ebae0b6a3ed62767c510b86c12a36c11fc8f7
Salatiel Bairros <salatiel.costa@totvs.com.br>
2025-11-18 15:32:40 -0300

    testando health com base api

---
+++ LoadBalancerTest/NewApp/Controllers/HealthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Repository;
+
+namespace NewApp.Controllers;
+
+[ApiController]
+[Route("api/balance/new/v1/[controller]")]
+public class HealthController : ControllerBase
+{
+    private readonly IInsertRecord _repository;
+
+    public HealthController(IInsertRecord repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet("check")]
+    public ActionResult<string> HealthCheck()
+    {
+        return Ok("HEALTHY");
+    }
+}
